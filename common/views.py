1a6224656f7c14de824e11eeca03899a2a7bd705
puzzle75 <puzzle75@naver.com>
2025-11-30 17:06:17 +0900

    404 페이지 추가

--- common/views.py
+++ common/views.py
@@ -2,9 +2,11 @@ from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from common.forms import UserForm
 
+
 def logout_view(request):
     logout(request)
     return redirect('index')
+
 
 def signup(request):
     if request.method == "POST":
@@ -19,3 +21,7 @@ def signup(request):
     else:
         form = UserForm()
     return render(request, 'common/signup.html', {'form': form})
+
+
+def page_not_found(request, exception):
+    return render(request, 'common/404.html', {})
